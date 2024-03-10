0a94551c9ef02f0a9cd7dc43f9a362e8c6ec5626
kesavaprasad08 <kesav2661998@gmail.com>
2024-03-10 15:20:30 +0530

    Mongoose

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const errorController = require('./controllers/error');
+const errorController = require("./controllers/error");
 
-const mongoConnect = require('./util/database').mongoConnect;
-const User= require('./models/user');
+// const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById('65ec4ed79bb64442a1f4e6bf')
-    .then(user => {
-      req.user = new User (user.name,user.email,user.cart,user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("65ec4ed79bb64442a1f4e6bf")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-
-mongoConnect(()=>{
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    "mongodb+srv://kesav:rollno1212@cluster0.cedis9y.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then((result) => {
+    app.listen(3000);
+  }).catch(err =>{
+    console.log(err)
+  })  ;
